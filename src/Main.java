cd966be70563596ceee8852363f7e3c52b0af437
Robin Kluis <faultic@gmail.com>
2023-01-04 12:46:18 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String greeting = System.getenv("GREETING");
+        String name = "World";
+        if (args.length > 0)
+            name = args[0];
+        System.out.println(greeting+" " +name+ "!");
+    }
+}
